4cc44ad281b0de7c717f4e327eaa34c38a051979
paliukhhalyna <g.palyukh@flex-solution.com>
2023-07-24 14:39:41 +0200

    Implemented Bin to Decimal
    Link: https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript

---
+++ level_8/binToDecimal.js
@@ -0,0 +1,10 @@
+/*REQUIREMENTS:
+Complete the function which converts a binary number (given as a string) to a decimal number.*/
+
+//SOLUTION:
+
+let binToDecimal = (bin)=> {
+    return parseInt(bin,2);
+};
+
+binToDecimal('1000'); //8
