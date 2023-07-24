bc3d1757439388997b9156fc21f56be1a6087876
paliukhhalyna <g.palyukh@flex-solution.com>
2023-07-24 11:56:57 +0200

    Fulfilled conversion from Hex to Decimal
    Link: https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript

---
+++ level_8/hexToDecimal.js
@@ -0,0 +1,8 @@
+/* REQUIREMENTS:
+Complete the function which converts hex number (given as a string) to a decimal number.*/
+
+function hexToDec(hex) {
+    return parseInt(hex,16);
+}
+
+hexToDec("FFF000");
