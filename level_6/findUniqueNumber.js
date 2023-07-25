5f8e98019b58cb7b8b67dbc6e3ff84e80cba5e56
paliukhhalyna <g.palyukh@flex-solution.com>
2023-07-25 19:45:42 +0200

    Added one more solution to Find the unique number
    Link: https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript

--- level_6/findUniqueNumber.js
+++ level_6/findUniqueNumber.js
@@ -5,16 +5,25 @@ It’s guaranteed that array contains at least 3 numbers.
 The tests contain some very huge arrays, so think about performance.*/
 
 
-//SOLUTION:
+//SOLUTIONS:
 function findUniq(arr) {
     let obj = {};
 
     arr.forEach(el => !obj[el] ? obj[el] = 1 : obj[el]++);
 
     for(let key in obj){
-        if(obj[key] == 1){
+        if(obj[key] === 1){
             return +key;
         }
     }
-
 }
+findUniq([0.5,1,1,1]);// 0.5
+
+// OR
+
+function findUniq2(arr) {
+    arr = arr.sort();
+    return arr[0]===arr[1]? arr.pop() : arr[0];
+}
+
+findUniq1([4,5,4,4,4]);// 5
