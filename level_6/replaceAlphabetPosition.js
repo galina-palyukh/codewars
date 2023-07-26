8dc809d59bc27dace4e62d6d09fb69a8a84e3645
paliukhhalyna <g.palyukh@flex-solution.com>
2023-07-26 19:43:22 +0200

    Implemented Replace With Alphabet Position
    Link: https://www.codewars.com/kata/546f922b54af40e1e90001da/train/javascript

---
+++ level_6/replaceAlphabetPosition.js
@@ -0,0 +1,42 @@
+/*DESCRIPTION:
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example:
+alphabetPosition("The sunset"); // "20 8 5 19 21 14 19" */
+
+// SOLUTIONS:
+function alphabetPosition1(text) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    let arr = text.split("");
+    let arrOnlyLetters = arr.filter(el => alphabet.includes(el.toLowerCase())).map(el => el.toLowerCase());
+    let indexes = [];
+    for (let i = 0; i < arrOnlyLetters.length; i++){
+        indexes.push(alphabet.indexOf(arrOnlyLetters[i])+1);
+    }
+    return indexes.join(" ");
+}
+
+// OR (with .replace and regexp)
+
+function alphabetPosition2(text) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    return text.toLowerCase().replace(/[^a-zA-Z]/gi,"").split("")
+        .map((el, i) => alphabet.indexOf(el)+1)
+        .join(" ")
+}
+
+// OR (with .test and regexp)
+
+function alphabetPosition3(text) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    return text.toLowerCase().split("")
+        .filter(el => /[a-z]/.test(el))
+        .map((el, i) => alphabet.indexOf(el)+1)
+        .join(" ")
+}
