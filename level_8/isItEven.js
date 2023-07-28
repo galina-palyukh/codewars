6e37a468e65dea2e533fdd3e3714bfbe5f910f7d
paliukhhalyna <g.palyukh@flex-solution.com>
2023-07-28 13:39:28 +0200

    Implemented Is it even?
    Link: https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript

---
+++ level_8/isItEven.js
@@ -0,0 +1,10 @@
+/*DESCRIOTION
+Find out whether a number is even and return true or false*/
+
+// SOLUTION:
+function testEven(n) {
+    return n%2===0;
+}
+testEven(5);//false
+testEven(2);//true
+
