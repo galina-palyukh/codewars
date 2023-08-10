f0d0d891dbebe2f9985ddb34d915999e5052fd0d
paliukhhalyna <g.palyukh@flex-solution.com>
2023-08-10 12:22:53 +0200

    Implemented Get the Middle Character
    Link: https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript

---
+++ level_7/GetMiddleCharacter.js
@@ -0,0 +1,20 @@
+/*DESCRIPTION:
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+Examples:
+
+getMiddle("test") should return "es"
+getMiddle("testing") should return "t"
+getMiddle("middle") should return "dd"
+getMiddle("A") should return "A"*/
+
+
+// SOLUTION:
+let getMiddle = (s) => {
+    let middle = Math.floor(s.length/2);
+    return s.length%2 === 0 ? s.substr(middle-1,2) : s.substr(middle,1);
+};
+
+
+console.log(getMiddle("abc")); // b
+console.log(getMiddle("abcd")); // bc
