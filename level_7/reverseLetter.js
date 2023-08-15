900934e1de720ade5daa7765dbde77e40f42af8e
paliukhhalyna <g.palyukh@flex-solution.com>
2023-08-15 19:30:20 +0200

    Added one more solution of "Reverse Letter" with regexp and match
    Link: https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/javascript

--- level_7/reverseLetter.js
+++ level_7/reverseLetter.js
@@ -6,9 +6,17 @@ For str = "krishan", the output should be "nahsirk".
 For str = "ultr53o?n", the output should be "nortlu".*/
 
 
-// SOLUTION:
+// SOLUTIONS:
 function reverseLetter(str) {
     return str.replace(/[^a-z]/gi,"").split("").reverse().join("");
 }
 
 console.log(reverseLetter("ultr53o?n"));//nortlu
+
+
+
+function reverseLetter2(str) {
+    return str.match(/[a-z]/gi).reverse().join("");
+}
+
+console.log(reverseLetter2("ab23c"));//cba
