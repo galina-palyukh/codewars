91d153aaa9b893f22db0247b7c0c122948d0231a
paliukhhalyna <g.palyukh@flex-solution.com>
2024-03-07 15:19:07 +0100

    Fixed No Loops 2 - You only need one

--- level_8/noLoops2-YouOnlyNeedOne.js
+++ level_8/noLoops2-YouOnlyNeedOne.js
@@ -4,8 +4,12 @@
 
 // Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
 
-// SOLUTION:
+
 check([66, 101], 66); //==> true;
 check([80, 117, 115, 104, 45, 85, 112, 115], 45); // ==> true;
 check(['t', 'e', 's', 't'], 'e'); // ==> true);
 check(['what', 'a', 'great', 'kata'], 'kat'); // ==> false);
+
+
+// SOLUTION:
+const check = (a,x) => a.includes(x);
