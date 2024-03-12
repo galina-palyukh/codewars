b7f4567170853b0ed827afb2ccb9841ec9ac53c6
paliukhhalyna <g.palyukh@flex-solution.com>
2024-03-12 15:13:58 +0100

    Merged differences Return the day

--- level_8/returnTheDay.js
+++ level_8/returnTheDay.js
@@ -16,9 +16,9 @@
 function whatday(num) { 
 
   const days = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
+
   return num > 0 && num <= 7 ? days[num - 1] : "Wrong, please enter a number between 1 and 7"
-  
+
 }
 
 // 2:
@@ -49,6 +49,6 @@ function whatday(num) {
     6: "Friday",
     7: "Saturday"
   }
-  
+
   return days[num] || "Wrong, please enter a number between 1 and 7";
 }
